646a1e227177a3fbb5f0dcc3c8703154abaf51e9
Mirza <katarza.21@gmail.com>
2022-02-16 17:45:52 +0100

    questions post request created

--- server/repositories/QuestionsRepository.js
+++ server/repositories/QuestionsRepository.js
@@ -1,5 +1,15 @@
+const db = require("../db");
+const QueryHelper = require("../Helpers/QueryHelper");
 const BaseRepository = require("./BaseRepository");
 
-class QuestionsRepository extends BaseRepository {}
+class QuestionsRepository extends BaseRepository {
+  post = async (body, parameterDb) => {
+    body.questionDate = new Date().toISOString();
+    console.log(body.questionDate);
+    const query = QueryHelper.generateInsertQuery(body, parameterDb);
+    const response = await db.query(query, Object.values(body));
+    return response.rows;
+  };
+}
 
 module.exports = QuestionsRepository;
