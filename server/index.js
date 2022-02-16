972eb4f6c840d2b7f238c18edb74f72bbd7d3a15
Mirza <katarza.21@gmail.com>
2022-02-16 11:59:20 +0100

    server project created

---
+++ server/index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+// MIDDLEWARE
+app.use(cors());
+app.use(express.json());
+
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => console.log(`Server running on localhost:${PORT}`));
