1f027f155b77b00226f137a775bb93e20fdf7771
Mirza <katarza.21@gmail.com>
2022-02-18 14:07:34 +0100

    client app created

---
+++ client/src/components/logo/Logo.js
@@ -0,0 +1,9 @@
+import React from "react";
+import logo from "../../assets/logo.png";
+import "./Logo.css";
+
+const Logo = () => {
+  return <img className="logo" alt="ask-it-logo" src={logo} />;
+};
+
+export default Logo;
