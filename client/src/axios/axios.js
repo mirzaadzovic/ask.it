ac267fd714a02e31b070474b5e09529026e29d65
Mirza <katarza.21@gmail.com>
2022-02-24 20:18:49 +0100

    axios base url production/development added

--- client/src/axios/axios.js
+++ client/src/axios/axios.js
@@ -1,7 +1,11 @@
 import axios from "axios";
 
+let api_route;
+if (process.env.NODE_ENV === "production") api_route = process.env.API_ROUTE;
+else api_route = "http://localhost:8080/api/v1";
+
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:8080/api/v1",
+  baseURL: api_route,
   withCredentials: true,
 });
 
